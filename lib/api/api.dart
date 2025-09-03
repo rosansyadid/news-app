0d4ef542efe3d196f5b27eb93d1c895422a79766
rosansyadid <awassb75@gmail.com>
2025-09-03 10:01:39 +0700

    feat: news searchbar

--- lib/api/api.dart
+++ lib/api/api.dart
@@ -3,10 +3,10 @@ import 'dart:convert';
 import 'package:http/http.dart' as http;
 
 class Api {
-  Future<List<Map<String, dynamic>>> getApi({String type = ''}) async {
+  Future<List<Map<String, dynamic>>> getApi({String category = ''}) async {
     String url = 'https://berita-indo-api.vercel.app/v1/cnn-news/';
-    if (type.isNotEmpty) {
-      url += type;
+    if (category.isNotEmpty) {
+      url += category;
     }
     final response = await http.get(Uri.parse(url));
 
--- lib/screen/api_screen.dart
+++ lib/screen/api_screen.dart
@@ -14,6 +14,50 @@ class ApiScreen extends StatefulWidget {
 
 class _ApiScreenState extends State<ApiScreen> {
   String _selectedCategory = '';
+  TextEditingController _searchController = TextEditingController();
+  List<Map<String, dynamic>> _allNews = [];
+  List<Map<String, dynamic>> _filteredNews = [];
+  bool isLoading = false;
+
+  Future<void> fetchNews(String type) async {
+    setState(() {
+      isLoading = true;
+    });
+    final data = await Api().getApi(category: type);
+    setState(() {
+      _allNews = data;
+      _filteredNews = data;
+      isLoading = false;
+    });
+  }
+
+  _applySearch(String query) {
+    setState(() {
+      if (query.isEmpty) {
+        setState(() {
+          _filteredNews = _allNews;
+        });
+      } else {
+        setState(() {
+          _filteredNews = _allNews.where((item) {
+            final title = item['title'].toString().toLowerCase();
+            final snippet = item['contentSnippet'].toString().toLowerCase();
+            final search = query.toLowerCase();
+            return title.contains(search) || snippet.contains(search);
+          }).toList();
+        });
+      }
+    });
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    fetchNews(_selectedCategory);
+    _searchController.addListener(() {
+      _applySearch(_searchController.text);
+    });
+  }
 
   Widget categoryButton(String label, String type) {
     return ElevatedButton(
@@ -40,6 +84,18 @@ class _ApiScreenState extends State<ApiScreen> {
       appBar: AppBar(title: Text('News App')),
       body: Column(
         children: [
+          TextField(
+            controller: _searchController,
+            decoration: InputDecoration(
+              prefixIcon: IconButton(
+                onPressed: () {
+                  _searchController.clear();
+                  _applySearch('');
+                },
+                icon: Icon(Icons.search),
+              ),
+            ),
+          ),
           SingleChildScrollView(
             scrollDirection: Axis.horizontal,
             child: Row(
@@ -62,129 +118,121 @@ class _ApiScreenState extends State<ApiScreen> {
           ),
 
           Expanded(
-            child: FutureBuilder(
-              future: Api().getApi(type: _selectedCategory),
-              builder: (context, snapshot) {
-                if (snapshot.connectionState == ConnectionState.waiting) {
-                  return Center(child: CircularProgressIndicator());
-                } else if (snapshot.hasError) {
-                  return Center(child: Text(snapshot.error.toString()));
-                }
+            child: isLoading
+                ? Center(child: CircularProgressIndicator())
+                : _filteredNews.isEmpty
+                ? Center(child: Text('No News Found'))
+                : ListView.builder(
+                    // Beri sedikit padding pada list agar kartu tidak menempel di tepi layar
+                    padding: const EdgeInsets.all(8.0),
+                    itemCount: _filteredNews.length,
+                    itemBuilder: (context, index) {
+                      final item = _filteredNews[index];
 
-                final data = snapshot.data!;
-                return ListView.builder(
-                  // Beri sedikit padding pada list agar kartu tidak menempel di tepi layar
-                  padding: const EdgeInsets.all(8.0),
-                  itemCount: data.length,
-                  itemBuilder: (context, index) {
-                    final item = data[index];
-
-                    // =======================================================================
-                    // BAGIAN INI YANG KITA UBAH DARI LISTTILE MENJADI CONTAINER
-                    // =======================================================================
-                    return InkWell(
-                      onTap: () {
-                        Navigator.push(
-                          context,
-                          MaterialPageRoute(
-                            builder: (context) =>
-                                DetailScreen(newsDetail: item),
+                      // =======================================================================
+                      // BAGIAN INI YANG KITA UBAH DARI LISTTILE MENJADI CONTAINER
+                      // =======================================================================
+                      return InkWell(
+                        onTap: () {
+                          Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                              builder: (context) =>
+                                  DetailScreen(newsDetail: item),
+                            ),
+                          );
+                        },
+                        // Beri efek sudut tumpul pada "splash" klik
+                        borderRadius: BorderRadius.circular(12),
+                        child: Container(
+                          // 1. STYLING KARTU
+                          margin: const EdgeInsets.symmetric(vertical: 8.0),
+                          padding: const EdgeInsets.all(12.0),
+                          decoration: BoxDecoration(
+                            color: Colors.white,
+                            borderRadius: BorderRadius.circular(12),
+                            boxShadow: [
+                              BoxShadow(
+                                color: Colors.black.withOpacity(0.05),
+                                spreadRadius: 1,
+                                blurRadius: 5,
+                                offset: const Offset(0, 3),
+                              ),
+                            ],
                           ),
-                        );
-                      },
-                      // Beri efek sudut tumpul pada "splash" klik
-                      borderRadius: BorderRadius.circular(12),
-                      child: Container(
-                        // 1. STYLING KARTU
-                        margin: const EdgeInsets.symmetric(vertical: 8.0),
-                        padding: const EdgeInsets.all(12.0),
-                        decoration: BoxDecoration(
-                          color: Colors.white,
-                          borderRadius: BorderRadius.circular(12),
-                          boxShadow: [
-                            BoxShadow(
-                              color: Colors.black.withOpacity(0.05),
-                              spreadRadius: 1,
-                              blurRadius: 5,
-                              offset: const Offset(0, 3),
-                            ),
-                          ],
-                        ),
-                        // 2. KONTEN DI DALAM KARTU
-                        child: Row(
-                          children: [
-                            // GAMBAR DI KIRI
-                            ClipRRect(
-                              borderRadius: BorderRadius.circular(8.0),
-                              child: Image.network(
-                                item['image']['small'],
-                                width: 100,
-                                height: 100,
-                                fit: BoxFit.cover,
-                                // (Opsional) Tampilkan loading saat gambar dimuat
-                                loadingBuilder: (context, child, progress) {
-                                  return progress == null
-                                      ? child
-                                      : Container(
-                                          width: 80,
-                                          height: 80,
-                                          color: Colors.grey[200],
-                                        );
-                                },
-                                // (Opsional) Tampilkan ikon jika gambar gagal dimuat
-                                errorBuilder: (context, error, stackTrace) {
-                                  return Container(
-                                    width: 80,
-                                    height: 80,
-                                    color: Colors.grey[200],
-                                    child: Icon(
-                                      Icons.broken_image,
-                                      color: Colors.grey[400],
-                                    ),
-                                  );
-                                },
+                          // 2. KONTEN DI DALAM KARTU
+                          child: Row(
+                            children: [
+                              // GAMBAR DI KIRI
+                              ClipRRect(
+                                borderRadius: BorderRadius.circular(8.0),
+                                child: Image.network(
+                                  item['image']['small'],
+                                  width: 100,
+                                  height: 100,
+                                  fit: BoxFit.cover,
+                                  // (Opsional) Tampilkan loading saat gambar dimuat
+                                  loadingBuilder: (context, child, progress) {
+                                    return progress == null
+                                        ? child
+                                        : Container(
+                                            width: 80,
+                                            height: 80,
+                                            color: Colors.grey[200],
+                                          );
+                                  },
+                                  // (Opsional) Tampilkan ikon jika gambar gagal dimuat
+                                  errorBuilder: (context, error, stackTrace) {
+                                    return Container(
+                                      width: 80,
+                                      height: 80,
+                                      color: Colors.grey[200],
+                                      child: Icon(
+                                        Icons.broken_image,
+                                        color: Colors.grey[400],
+                                      ),
+                                    );
+                                  },
+                                ),
                               ),
-                            ),
-                            const SizedBox(width: 12),
+                              const SizedBox(width: 12),
 
-                            // TEKS DI KANAN
-                            Expanded(
-                              child: Column(
-                                crossAxisAlignment: CrossAxisAlignment.start,
-                                children: [
-                                  Text(
-                                    item['title'],
-                                    maxLines:
-                                        2, // Beri 2 baris agar judul lebih terbaca
-                                    overflow: TextOverflow.ellipsis,
-                                    style: const TextStyle(
-                                      fontWeight: FontWeight.bold,
-                                      fontSize: 16,
+                              // TEKS DI KANAN
+                              Expanded(
+                                child: Column(
+                                  crossAxisAlignment: CrossAxisAlignment.start,
+                                  children: [
+                                    Text(
+                                      item['title'],
+                                      maxLines:
+                                          2, // Beri 2 baris agar judul lebih terbaca
+                                      overflow: TextOverflow.ellipsis,
+                                      style: const TextStyle(
+                                        fontWeight: FontWeight.bold,
+                                        fontSize: 16,
+                                      ),
                                     ),
-                                  ),
-                                  const SizedBox(height: 8),
-                                  Text(
-                                    formatDate(item['isoDate']),
-                                    // Anda bisa format ini nanti
-                                    maxLines: 1,
-                                    overflow: TextOverflow.ellipsis,
-                                    style: TextStyle(
-                                      color: Colors.grey[600],
-                                      fontSize: 12,
+                                    const SizedBox(height: 8),
+                                    Text(
+                                      formatDate(item['isoDate']),
+                                      // Anda bisa format ini nanti
+                                      maxLines: 1,
+                                      overflow: TextOverflow.ellipsis,
+                                      style: TextStyle(
+                                        color: Colors.grey[600],
+                                        fontSize: 12,
+                                      ),
                                     ),
-                                  ),
-                                ],
+                                  ],
+                                ),
                               ),
-                            ),
-                          ],
+                            ],
+                          ),
                         ),
-                      ),
-                    );
-                    // =======================================================================
-                  },
-                );
-              },
-            ),
+                      );
+                      // =======================================================================
+                    },
+                  ),
           ),
         ],
       ),
